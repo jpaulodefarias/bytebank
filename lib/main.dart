38f9629fcff8f4b3f0b0a6d54b6e1fd67c13eeed
João Paulo de Farias <jpaulodefarias@gmail.com>
2020-08-19 05:55:44 -0300

    Refactoring App

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,58 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
+void main() => runApp(App());
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
-        body: Column(
-          children: [
-            Card(
-              child: ListTile(
-                leading: Icon(Icons.monetization_on),
-                title: Text('100.0'),
-                subtitle: Text('1000'),
-              ),
-            ),
-            Card(
-              child: ListTile(
-                leading: Icon(Icons.monetization_on),
-                title: Text('200.0'),
-                subtitle: Text('1000'),
-              ),
-            ),
-          ],
-        ),
+        body: TransferList(),
         appBar: AppBar(
           title: Text('Transfers'),
         ),
         floatingActionButton: FloatingActionButton(
+          onPressed: () {},
           child: Icon(Icons.add),
         ),
       ),
-    ));
+    );
+  }
+}
+
+class TransferList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        TransferItem(Transfer(100.0, 1000)),
+        TransferItem(Transfer(200.0, 2000)),
+        TransferItem(Transfer(300.0, 3000)),
+      ],
+    );
+  }
+}
+
+class TransferItem extends StatelessWidget {
+  final Transfer _transfer;
+
+  TransferItem(this._transfer);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        leading: Icon(Icons.monetization_on),
+        title: Text(_transfer.amount.toString()),
+        subtitle: Text(_transfer.accountNumber.toString()),
+      ),
+    );
+  }
+}
+
+class Transfer {
+  final double amount;
+  final int accountNumber;
+
+  Transfer(this.amount, this.accountNumber);
+}
