60e0fd9f10e4b0882b448b8f20cf3777ad3b0c77
João Paulo de Farias <jpaulodefarias@gmail.com>
2021-01-15 18:46:44 -0300

    Refactoring TransferForm

--- lib/main.dart
+++ lib/main.dart
@@ -57,9 +57,18 @@ class Transfer {
   final int accountNumber;
 
   Transfer(this.amount, this.accountNumber);
+
+  @override
+  String toString() {
+    return 'Transfer{amount: $amount, accountNumber: $accountNumber}';
+  }
 }
 
 class TransferForm extends StatelessWidget {
+  final TextEditingController _accountNumberController =
+      TextEditingController();
+  final TextEditingController _amountController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -68,38 +77,63 @@ class TransferForm extends StatelessWidget {
       ),
       body: Column(
         children: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: TextField(
-              style: TextStyle(
-                fontSize: 24.0,
-              ),
-              decoration: InputDecoration(
-                labelText: 'Account number',
-                hintText: '0000',
-              ),
-              keyboardType: TextInputType.number,
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: TextField(
-              style: TextStyle(
-                fontSize: 24.0,
-              ),
-              decoration: InputDecoration(
-                icon: Icon(Icons.monetization_on),
-                labelText: 'Amount',
-                hintText: '0.00',
-              ),
-              keyboardType: TextInputType.number,
-            ),
-          ),
+          TextEditor(
+              controller: _accountNumberController,
+              label: 'Account number',
+              hint: '0000'),
+          TextEditor(
+              controller: _amountController,
+              label: 'Amount',
+              hint: '0.00',
+              icon: Icons.monetization_on),
           RaisedButton(
-            onPressed: () {},
+            onPressed: () => _createTransfer(context),
             child: Text('Confirm'),
           )
         ],
+      ),
+    );
+  }
+
+  void _createTransfer(BuildContext context) {
+    final int accountNumber = int.tryParse(_accountNumberController.text);
+    final double amount = double.tryParse(_amountController.text);
+
+    if (accountNumber != null && amount != null) {
+      final newTransfer = Transfer(amount, accountNumber);
+
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text('$newTransfer'),
+        ),
+      );
+    }
+  }
+}
+
+class TextEditor extends StatelessWidget {
+  final TextEditingController controller;
+  final String label;
+  final String hint;
+  final IconData icon;
+
+  TextEditor({this.controller, this.label, this.hint, this.icon});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: TextField(
+        controller: controller,
+        style: TextStyle(
+          fontSize: 24.0,
+        ),
+        decoration: InputDecoration(
+          icon: icon != null ? Icon(icon) : null,
+          labelText: label,
+          hintText: hint,
+        ),
+        keyboardType: TextInputType.number,
       ),
     );
   }
