1a8d5ce928ecb72042089f2e2281c51a11af04b9
João Paulo de Farias <jpaulodefarias@gmail.com>
2021-01-17 17:44:13 -0300

    Refactoring App

--- lib/main.dart
+++ lib/main.dart
@@ -6,9 +6,7 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        body: TransferList(),
-      ),
+      home: TransferList()
     );
   }
 }
